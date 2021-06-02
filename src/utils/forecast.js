86b884a142fb4f17c9ed35a34ab539e91a5b811c
philm89 <philm892@gmail.com>
2021-06-02 14:55:29 +0700

    Changed degree text font

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -19,11 +19,11 @@ const forecast = (lat, long, callback) => {
         body.current.weather_descriptions[0] +
           ' It is currently ' +
           body.current.temperature +
-          ' ℃ out. The current wind speed is ' +
+          ' °C out. The current wind speed is ' +
           body.current.wind_speed +
           ' KPH. It feels like it is ' +
           body.current.feelslike +
-          ' ℃ out.'
+          ' °C out.'
       );
     }
   });
